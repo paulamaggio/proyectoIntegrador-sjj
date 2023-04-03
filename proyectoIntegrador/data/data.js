465648227b8ecdb705b00a590d62bf0ff3fc97e4
JustinaVergara <vergaram@udesa.edu.ar>
2023-04-03 19:50:12 -0300

    modulo de datos y forms

---
+++ proyectoIntegrador/data/data.js
@@ -0,0 +1,18 @@
+const data = {
+    usuario: { 
+        email : "jeregomez@gmail.com",
+        usuario: "jere_gomez",
+        contraseña: "Jeremias123",
+        nacimiento: 13/4/2003,
+        dni : 45978253,
+        foto : "./public/images/users/foto_jeremias/jpg"
+    },
+    productos: [
+
+    ],
+    comentarios: [
+
+    ]
+}
+
+module.exports = data 
