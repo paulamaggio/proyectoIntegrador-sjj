bf4801f131a71e3f9e86195cbb1b624482704330
juanneira16 <jneira@udesa.edu.ar>
2023-04-05 08:16:26 -0300

    Carpeta de modulo de datos terminada, con productos y comentarios

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -8,10 +8,65 @@ const data = {
         foto : "./public/images/users/foto_jeremias/jpg"
     },
     productos: [
-
+        {   nombre: 'I will be right here',
+            descripcion: 'Remera con frase de ET, color celeste ',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'ET Long Sleeve',
+            descripcion:'Remera con frase de ET, manga larga, color azul ',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Peluche ET con camperita',
+            descripcion: 'Peluche de ET para decorar o jugar con el',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Peluche ET con corazon',
+            descripcion: 'Peluche del 40 aniversario de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Peluche ET en bata',
+            descripcion: 'Peluche de ET con bata, super suave y tierno',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Peluche ET Bebe',
+            descripcion: 'Peluche super suave y tierno de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Set de vasos de shot de ET',
+            descripcion: 'Vasos de shot del aniversario 40 de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Pin The extra-terrestrial ',
+            descripcion: 'Pin coleccionable de la tienda del mejor extraterestre',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Magnet I love ET',
+            descripcion: 'Iman para decorar tu heladera de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'Magnet Be Good',
+            descripcion: 'Iman para decorar tu heladera de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'ET Wallet',
+            descripcion: 'Billetera de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'ET Mug',
+            descripcion: 'Taza con doble diseño de ET',
+            fechaDeCarga: ''
+        },
+        {   nombre: 'ET Key Ring',
+            descripcion: 'Llaver de ET con la figura de ET en 3D, para decorar tus llaves de la casa, del auto y mas',
+            fechaDeCarga: ''
+        },
     ],
     comentarios: [
-
+        {
+        nombreDeUsuario: "jere_gomez",
+        textoDeComentario: 'El producto es increible, lo recomiendo mucho, de muy buena calidad',
+        imagenDePerfil: './public/images/users/foto_jeremias/jpg',
+        },
     ]
 }
 
