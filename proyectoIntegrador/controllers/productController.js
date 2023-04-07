232b59e2d29383791ec0954d580c6c754b32ff64
paulamaggio <pmaggio@udesa.edu.ar>
2023-04-07 20:31:58 -0300

    index, productos, producto con info db. JUAN JUSTI SOFIA

--- proyectoIntegrador/controllers/productController.js
+++ proyectoIntegrador/controllers/productController.js
@@ -2,13 +2,13 @@ const data = require('../data/data');
 
 const productController = {
     product: function (req,res) {
-        return res.render('product')
+        return res.render('product', {data: data})
     },
     productAdd: function (req,res) {
         return res.render('product-add')
     },
     products: function (req,res) {
-        return res.render('products')
+        return res.render('products', {data: data.productos})
     },
 }
 
--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -5,70 +5,70 @@ const data = {
         contraseña: "Jeremias123",
         nacimiento: 13/4/2003,
         dni : 45978253,
-        foto : "./public/images/users/foto_jeremias/jpg"
+        foto : "/images/users/foto_jeremias.jpg"
     },
     productos: [
-        {   fotoProducto: './public/images/products/remeraCortaET/webp',
+        {   fotoProducto: '/images/products/remeraCortaET.webp',
             nombre: 'I will be right here',
             descripcion: 'Remera con frase de ET, color celeste ',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/remeraLargaET/jpg',
+        {   fotoProducto: '/images/products/remeraLargaET.jpg',
             nombre: 'ET Long Sleeve',
             descripcion:'Remera con frase de ET, manga larga, color azul ',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/pelucheETcamperita/jpg',
+        {   fotoProducto: '/images/products/pelucheETcamperita.jpg',
             nombre: 'Peluche ET con camperita',
             descripcion: 'Peluche de ET para decorar o jugar con el',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/pelucheETcorazon/jpg',
+        {   fotoProducto: '/images/products/pelucheETcorazon.jpg',
             nombre: 'Peluche ET con corazon',
             descripcion: 'Peluche del 40 aniversario de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/pelucheETbata/jpg',
+        {   fotoProducto: '/images/products/pelucheETbata.jpg',
             nombre: 'Peluche ET en bata',
             descripcion: 'Peluche de ET con bata, super suave y tierno',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/pelucheETbebe/jpg',
+        {   fotoProducto: '/images/products/pelucheETbebe.jpg',
             nombre: 'Peluche ET Bebe',
             descripcion: 'Peluche super suave y tierno de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/shotsET/png',
+        {   fotoProducto: '/images/products/shotsET.png',
             nombre: 'Set de vasos de shot de ET',
             descripcion: 'Vasos de shot del aniversario 40 de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/pinET/jpg',
+        {   fotoProducto: '/images/products/pinET.jpg',
             nombre: 'Pin The extra-terrestrial ',
             descripcion: 'Pin coleccionable de la tienda del mejor extraterestre',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/imanILET/png',
+        {   fotoProducto: '/images/products/imanILET.png',
             nombre: 'Magnet I love ET',
             descripcion: 'Iman para decorar tu heladera de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/imanBeGoodET/png',
+        {   fotoProducto: '/images/products/imanBeGoodET.png',
             nombre: 'Magnet Be Good',
             descripcion: 'Iman para decorar tu heladera de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/billeteraET/jpg',
+        {   fotoProducto: '/images/products/billeteraET.jpg',
             nombre: 'ET Wallet',
             descripcion: 'Billetera de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/tazaET/webp',
+        {   fotoProducto: '/images/products/tazaET.webp',
             nombre: 'ET Mug',
             descripcion: 'Taza con doble diseño de ET',
             fechaDeCarga: ''
         },
-        {   fotoProducto: './public/images/products/llaveroETcamperita/jpg',
+        {   fotoProducto: '/images/products/llaveroETcamperita.jpg',
             nombre: 'ET Key Ring',
             descripcion: 'Llavero de ET con la figura de ET en 3D, para decorar tus llaves de la casa, del auto y mas',
             fechaDeCarga: ''
@@ -77,8 +77,28 @@ const data = {
     comentarios: [
         {
         nombreDeUsuario: "jere_gomez",
-        textoDeComentario: 'El producto es increible, lo recomiendo mucho, de muy buena calidad',
-        imagenDePerfil: './public/images/users/foto_jeremias/jpg',
+        textoDeComentario: 'El producto es increible',
+        imagenDePerfil: '/images/users/foto_jeremias.jpg',
+        },
+        {
+        nombreDeUsuario: "bocapasion",
+        textoDeComentario: 'Podria tener un buzo de boca?',
+        imagenDePerfil: '/images/users/foto_jeremias.jpg',
+        },
+        {
+        nombreDeUsuario: "sofimaggio",
+        textoDeComentario: 'Me encanta ET',
+        imagenDePerfil: '/images/users/foto_jeremias.jpg',
+        },
+        {
+        nombreDeUsuario: "lolo_123",
+        textoDeComentario: 'Tienen tazas de ET?',
+        imagenDePerfil: '/images/users/foto_jeremias.jpg',
+        },
+        {
+        nombreDeUsuario: "justivergara",
+        textoDeComentario: 'Traje mi peluche a Usuahia',
+        imagenDePerfil: '/images/users/foto_jeremias.jpg',
         },
     ]
 }
