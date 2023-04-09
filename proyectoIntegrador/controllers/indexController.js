34a76ef7f3f590806755886c63323ed9aea6ff1d
JustinaVergara <vergaram@udesa.edu.ar>
2023-04-08 22:04:43 -0300

    controller index error

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -2,10 +2,10 @@ const data = require('../data/data');
 
 const indexController = {
     headerLogueado: function (req,res) {
-        return res.render('headerLogueado');
+        return res.render('headerLogueado', {data: data});
     },
     index: function (req,res) {
-        return res.render('index', {data: data});
+        return res.render('index', {data: data.productos});
     },
     search: function (req,res) {
         return res.render('search-results');
