00e814fa8a492af216f4e77d93917e11da05f99d
paulamaggio <pmaggio@udesa.edu.ar>
2023-06-07 23:45:33 -0300

    me olvide de borrar unas cosas

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -26,28 +26,6 @@ const indexController = {
     },
 
     search: function (req,res) {
-        // Producto.findAll({
-        //     where:[{nombreProducto:{[op.like]:'%'+req.query.busqueda+'%'}}],
-        //     include: [{association:'usuarios'}, {association:'comentarios'}]
-        // })
-        // .then(function(data){
-        //     return res.render('search-results', {data: data});
-        // })
-        // .catch(function(err){
-        //     console.log(err);
-        // })
-
-        // let busqueda = req.query.busqueda;
-        // let relaciones = {include: [{association:'usuarios'}, {association:'comentarios'}]};
-        // let criterio = {where:[{nombreProducto:{[op.like]:'%'+busqueda+'%'}}]}
-        // Producto.findAll(criterio,relaciones)
-        // .then(function(data){
-        //     // return res.send(data);
-        //     // return res.render('search-results', {data: data});
-        // })
-        // .catch(function(err){
-        //     console.log(err);
-        // })
 
         let busqueda = req.query.search;
         console.log(busqueda);
